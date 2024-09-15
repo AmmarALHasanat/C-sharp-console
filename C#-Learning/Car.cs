981d7d7460c2c0f5e9f744624fab2a492202c1a4
Ammar AL-Hasanat <ammarabed13@gmail.com>
2024-09-15 10:59:46 +0300

    add new class

---
+++ C#-Learning/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    internal class Car
+    {
+        private string name;
+        public Car()
+        {
+            name = "";
+        }
+
+        public string getName() {
+            return name;
+         }
+        public void setName(string var)
+        {
+            name = var;
+        }
+    }
+}
--- C#-Learning/Program.cs
+++ C#-Learning/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
+using Car;
+// name space for using in other file like
 namespace HelloWorld
 {
+    /**
+     * This is a comment
+     */
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // This is a Car Object
+            Car.Car car = new Car.Car();
+            car.setName("BMW");
+            Console.WriteLine(car.getName());
         }
     }
 }
